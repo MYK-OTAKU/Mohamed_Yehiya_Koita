33169900377bcf0961e7e1973f381e973b4de4b3
KINGMYK1 <mohamedyehiyakoita@gmail.com>
2025-04-26 15:57:55 +0100

    Updates

--- assets/Contact-BJVRPbBD.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CUjK8Zxw.js";const o=()=>t.jsx("div",{children:t.jsx("h1",{children:"Welcome to the Contact Page"})});export{o as default};
---
+++ assets/Contact-WRijJr8n.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C8GkWMk-.js";const o=()=>t.jsx("div",{children:t.jsx("h1",{children:"Welcome to the Contact Page"})});export{o as default};
